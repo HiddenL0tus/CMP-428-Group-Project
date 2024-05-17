b77f5470635712ee1b866d35c446ca454f7170db
Omar Maldonado <omar.maldonado1@lc.cuny.edu>
2024-05-16 20:50:55 -0400

    cleanup and removal of unused variables

--- src/main/GameS24.java
+++ src/main/GameS24.java
@@ -37,17 +37,10 @@ public class GameS24 extends GameBase
 	Player player;
 	Orc[] orcList;
 	
-	Potion[] potions;
-	Rect2[] walls, doors;
-	
-	boolean Q_pressed = false;
-	boolean E_pressed = false;
+	Rect2[] walls;
 	
 	public void initialize()
-	{	
-		
-		
-        
+	{	 
 		gd = GraphicsEnvironment.getLocalGraphicsEnvironment().getDefaultScreenDevice();
 		SCREEN_WIDTH    = gd.getDisplayMode().getWidth ();
 		SCREEN_HEIGHT   = gd.getDisplayMode().getHeight();
@@ -61,19 +54,11 @@ public class GameS24 extends GameBase
 		playerProjectiles = new ArrayList<>();
 		items             = new ArrayList<>();
 		
+		items.add(new Potion("Health", 597, 400));
+		items.add(new Potion("Immunity", 630, 400));
+		items.add(new Potion("Health", 750, 400));
 		items.add(new WoodSword(1300, 1000));
 		items.add(new WoodBow(1350, 1000));
-		
-		items.add(new Potion("Health", 2657, 2081));
-		items.add(new Potion("Health", 2068, 1180));
-		items.add(new Potion("Health", 3084, 1168));
-		items.add(new Potion("Health", 1306,  518));
-		items.add(new Potion("Health", 4650, 2445));
-		
-		items.add(new Potion("Immunity", 736, 2081));
-		items.add(new Potion("Immunity", 402,  595));
-		items.add(new Potion("Immunity", 4663, 734));
-		
 		
 		player = new Player(2700, 2400);
 			
@@ -89,121 +74,7 @@ public class GameS24 extends GameBase
 		
 		for (Orc orcBaddy : orcList) baddies.add(orcBaddy);
 		
-		walls   = new Rect2[]
-		{
-			new Rect2(1920, 3, 960, 144),
-			new Rect2(3840, 3, 960, 144),
-			new Rect2(48, 336, 96, 1680),
-			new Rect2(48, 336, 1968, 144),
-			new Rect2(1920, 3, 96, 480),
-			new Rect2(2784, 3, 96, 480),
-			new Rect2(2784, 336, 240, 144),
-			new Rect2(2928, 144, 96, 336),
-			new Rect2(2928, 144, 912, 144),
-			new Rect2(3744, 96, 96, 432),
-			new Rect2(4704, 3, 96, 4752),
-			new Rect2(1920, 1824, 432, 240),
-			new Rect2(2448, 1824, 432, 240),
-			new Rect2(1920, 624, 96, 2640),
-			new Rect2(2784, 624, 96, 2640),
-			new Rect2(1920, 2208, 240, 144),
-			new Rect2(2256, 2208, 624, 144),
-			new Rect2(1920, 2784, 960, 96),
-			new Rect2(2400, 2208, 96, 432),
-			new Rect2(1920, 1344, 720, 192),
-			new Rect2(2736, 1344, 48, 192),
-			new Rect2(1920, 864, 432, 240),
-			new Rect2(2448, 864, 432, 240),
-			new Rect2(2304, 384, 192, 144),
-			new Rect2(2784, 624, 240, 336),
-			new Rect2(2880, 960, 384, 144),
-			new Rect2(3168, 960, 96, 912),
-			new Rect2(3360, 1056, 96, 192),
-			new Rect2(3360, 1392, 96, 192),
-			new Rect2(3744, 480, 336, 240),
-			new Rect2(3840, 3, 96, 576),
-			new Rect2(4176, 480, 624, 144),
-			new Rect2(4176, 768, 624, 144),
-			new Rect2(4176, 480, 96, 576),
-			new Rect2(3552, 960, 720, 144),
-			new Rect2(3552, 960, 96, 624),
-			new Rect2(3552, 1248, 624, 144),
-			new Rect2(4080, 960, 96, 288),
-			new Rect2(3168, 1728, 1248, 144),
-			new Rect2(4512, 1728, 288, 240),
-			new Rect2(3648, 1728, 96, 432),
-			new Rect2(4320, 1728, 96, 288),
-			new Rect2(2880, 2784, 432, 144),
-			new Rect2(3216, 2448, 528, 144),
-			new Rect2(3648, 2304, 96, 288),
-			new Rect2(3216, 2448, 96, 432),
-			new Rect2(3408, 2784, 1404, 144),
-			new Rect2(3744, 2496, 240, 48),
-			new Rect2(4080, 2496, 624, 48),
-			new Rect2(3840, 2784, 96, 1056),
-			new Rect2(2784, 3840, 1152, 144),
-			new Rect2(2496, 3408, 384, 480),
-			new Rect2(1920, 3408, 384, 480),
-			new Rect2(2304, 3408, 48, 144),
-			new Rect2(2448, 3408, 48, 144),
-			new Rect2(2448, 3744, 48, 144),
-			new Rect2(2304, 3744, 48, 144),
-			new Rect2(2352, 3456, 96, 96),
-			new Rect2(2352, 3792, 96, 96),
-			new Rect2(2256, 2832, 96, 432),
-			new Rect2(2448, 2832, 96, 432),
-			new Rect2(1920, 3120, 432, 144),
-			new Rect2(2448, 3120, 432, 144),
-			new Rect2(864, 3840, 1152, 144),
-			new Rect2(1440, 3840, 96, 960),
-			new Rect2(1440, 4704, 1920, 96),
-			new Rect2(3264, 3840, 96, 432),
-			new Rect2(3264, 4368, 96, 432),
-			new Rect2(3264, 4032, 768, 192),
-			new Rect2(3936, 3888, 864, 144),
-			new Rect2(3264, 4368, 768, 192),
-			new Rect2(3936, 4560, 864, 96),
-			new Rect2(1920, 4128, 192, 144),
-			new Rect2(2688, 4128, 192, 144),
-			new Rect2(1632, 4368, 192, 144),
-			new Rect2(2304, 4368, 192, 144),
-			new Rect2(2976, 4368, 192, 144),
-			new Rect2(864, 2928, 96, 912),
-			new Rect2(864, 2784, 528, 144),
-			new Rect2(1488, 2784, 528, 144),
-			new Rect2(240, 2928, 672, 96),
-			new Rect2(240, 1824, 96, 1200),
-			new Rect2(48, 1824, 1344, 144),
-			new Rect2(240, 2112, 768, 144),
-			new Rect2(1152, 2112, 240, 144),
-			new Rect2(768, 1824, 96, 384),
-			new Rect2(1296, 1824, 96, 384),
-			new Rect2(240, 2496, 240, 144),
-			new Rect2(624, 2496, 1296, 144),
-			new Rect2(1824, 2112, 96, 480),
-			new Rect2(1488, 2112, 432, 144),
-			new Rect2(1488, 1824, 336, 432),
-			new Rect2(1728, 624, 192, 1296),
-			new Rect2(240, 1488, 1392, 144),
-			new Rect2(1536, 1488, 96, 336),
-			new Rect2(240, 1200, 576, 144),
-			new Rect2(912, 1200, 672, 144),
-			new Rect2(1488, 1296, 96, 96),
-			new Rect2(144, 912, 192, 144),
-			new Rect2(432, 912, 768, 144),
-			new Rect2(1296, 912, 432, 144),
-			new Rect2(1584, 1056, 144, 192),
-			new Rect2(240, 624, 480, 144),
-			new Rect2(816, 624, 768, 144),
-			new Rect2(432, 432, 96, 240),
-			new Rect2(1344, 432, 96, 240),
-			new Rect2(1344, 432, 96, 240),
-		};
-		
-		doors   = new Rect2[]
-		{
-			//new Rect2( 500, 500, 300, 300)
-		};
+		createWalls();
 		
 		int cameraX = player.x - (SCREEN_WIDTH / 2) + (player.w / 2);
 	    int cameraY = player.y - (SCREEN_HEIGHT / 2) + (player.h / 2);
@@ -230,22 +101,6 @@ public class GameS24 extends GameBase
 		}
 		
 		pickupItems(player);
-		
-		if(pressing[_Q]) {
-			if(!Q_pressed) player.useItem(0); 
-			Q_pressed = true;
-		}
-		else {
-			Q_pressed = false;
-		}
-		
-		if(pressing[_E]) {
-			if(!E_pressed) player.useItem(1);
-			E_pressed = true;
-		}
-		else {
-			E_pressed = false;
-		}
 		
 		controlMovement();
 		
@@ -291,9 +146,7 @@ public class GameS24 extends GameBase
 				}
 			}
 		}
-		
 	}
-	
 	
 	public void controlMovement() {
 		int moveSpeed = 3;
@@ -424,8 +277,6 @@ public class GameS24 extends GameBase
         
 		//for (Rect2 wall : walls) wall.draw(pen);
 		
-		for (Rect2 door : doors) door.draw(pen);
-		
 		for (Item  item : items) item.draw(pen);
 		
 		
@@ -462,12 +313,6 @@ public class GameS24 extends GameBase
 			else if(wall.held) wall.moveBy(dx, dy);
 		}
 		
-		for(Rect2 door : doors) 
-		{
-			if(door.resizer.held) door.resizeBy(dx,  dy);
-			else if(door.held) door.moveBy(dx, dy);
-		}
-		
 		mx = nx;
 		my = ny;
 	}
@@ -482,14 +327,6 @@ public class GameS24 extends GameBase
 			if(wall.contains(mx,  my)) wall.grabbed();
 			if(wall.resizer.contains(mx,  my)) wall.resizer.grabbed();
 		}
-		
-		for(Rect2 door : doors) 
-		{
-			if(door.contains(mx,  my)) door.grabbed();
-			if(door.resizer.contains(mx,  my))  door.resizer.grabbed();
-		}
-		
-		System.out.println("" + mx + ", " + my);
 	}
 	
 	public void mouseReleased(MouseEvent e) 
@@ -499,14 +336,122 @@ public class GameS24 extends GameBase
 			wall.dropped();
 			wall.resizer.dropped();
 		}
-		
-		for(Rect2 door: doors) 
-		{
-			door.dropped();
-			door.resizer.dropped();
-		}
 	}
 	//TOOL for RESIZING RECTS ENDS*/
+	
+	public void createWalls()
+	{
+		walls   = new Rect2[]
+				{
+					new Rect2(1920, 3, 960, 144),
+					new Rect2(3840, 3, 960, 144),
+					new Rect2(48, 336, 96, 1680),
+					new Rect2(48, 336, 1968, 144),
+					new Rect2(1920, 3, 96, 480),
+					new Rect2(2784, 3, 96, 480),
+					new Rect2(2784, 336, 240, 144),
+					new Rect2(2928, 144, 96, 336),
+					new Rect2(2928, 144, 912, 144),
+					new Rect2(3744, 96, 96, 432),
+					new Rect2(4704, 3, 96, 4752),
+					new Rect2(1920, 1824, 432, 240),
+					new Rect2(2448, 1824, 432, 240),
+					new Rect2(1920, 624, 96, 2640),
+					new Rect2(2784, 624, 96, 2640),
+					new Rect2(1920, 2208, 240, 144),
+					new Rect2(2256, 2208, 624, 144),
+					new Rect2(1920, 2784, 960, 96),
+					new Rect2(2400, 2208, 96, 432),
+					new Rect2(1920, 1344, 720, 192),
+					new Rect2(2736, 1344, 48, 192),
+					new Rect2(1920, 864, 432, 240),
+					new Rect2(2448, 864, 432, 240),
+					new Rect2(2304, 384, 192, 144),
+					new Rect2(2784, 624, 240, 336),
+					new Rect2(2880, 960, 384, 144),
+					new Rect2(3168, 960, 96, 912),
+					new Rect2(3360, 1056, 96, 192),
+					new Rect2(3360, 1392, 96, 192),
+					new Rect2(3744, 480, 336, 240),
+					new Rect2(3840, 3, 96, 576),
+					new Rect2(4176, 480, 624, 144),
+					new Rect2(4176, 768, 624, 144),
+					new Rect2(4176, 480, 96, 576),
+					new Rect2(3552, 960, 720, 144),
+					new Rect2(3552, 960, 96, 624),
+					new Rect2(3552, 1248, 624, 144),
+					new Rect2(4080, 960, 96, 288),
+					new Rect2(3168, 1728, 1248, 144),
+					new Rect2(4512, 1728, 288, 240),
+					new Rect2(3648, 1728, 96, 432),
+					new Rect2(4320, 1728, 96, 288),
+					new Rect2(2880, 2784, 432, 144),
+					new Rect2(3216, 2448, 528, 144),
+					new Rect2(3648, 2304, 96, 288),
+					new Rect2(3216, 2448, 96, 432),
+					new Rect2(3408, 2784, 1404, 144),
+					new Rect2(3744, 2496, 240, 48),
+					new Rect2(4080, 2496, 624, 48),
+					new Rect2(3840, 2784, 96, 1056),
+					new Rect2(2784, 3840, 1152, 144),
+					new Rect2(2496, 3408, 384, 480),
+					new Rect2(1920, 3408, 384, 480),
+					new Rect2(2304, 3408, 48, 144),
+					new Rect2(2448, 3408, 48, 144),
+					new Rect2(2448, 3744, 48, 144),
+					new Rect2(2304, 3744, 48, 144),
+					new Rect2(2352, 3456, 96, 96),
+					new Rect2(2352, 3792, 96, 96),
+					new Rect2(2256, 2832, 96, 432),
+					new Rect2(2448, 2832, 96, 432),
+					new Rect2(1920, 3120, 432, 144),
+					new Rect2(2448, 3120, 432, 144),
+					new Rect2(864, 3840, 1152, 144),
+					new Rect2(1440, 3840, 96, 960),
+					new Rect2(1440, 4704, 1920, 96),
+					new Rect2(3264, 3840, 96, 432),
+					new Rect2(3264, 4368, 96, 432),
+					new Rect2(3264, 4032, 768, 192),
+					new Rect2(3936, 3888, 864, 144),
+					new Rect2(3264, 4368, 768, 192),
+					new Rect2(3936, 4560, 864, 96),
+					new Rect2(1920, 4128, 192, 144),
+					new Rect2(2688, 4128, 192, 144),
+					new Rect2(1632, 4368, 192, 144),
+					new Rect2(2304, 4368, 192, 144),
+					new Rect2(2976, 4368, 192, 144),
+					new Rect2(864, 2928, 96, 912),
+					new Rect2(864, 2784, 528, 144),
+					new Rect2(1488, 2784, 528, 144),
+					new Rect2(240, 2928, 672, 96),
+					new Rect2(240, 1824, 96, 1200),
+					new Rect2(48, 1824, 1344, 144),
+					new Rect2(240, 2112, 768, 144),
+					new Rect2(1152, 2112, 240, 144),
+					new Rect2(768, 1824, 96, 384),
+					new Rect2(1296, 1824, 96, 384),
+					new Rect2(240, 2496, 240, 144),
+					new Rect2(624, 2496, 1296, 144),
+					new Rect2(1824, 2112, 96, 480),
+					new Rect2(1488, 2112, 432, 144),
+					new Rect2(1488, 1824, 336, 432),
+					new Rect2(1728, 624, 192, 1296),
+					new Rect2(240, 1488, 1392, 144),
+					new Rect2(1536, 1488, 96, 336),
+					new Rect2(240, 1200, 576, 144),
+					new Rect2(912, 1200, 672, 144),
+					new Rect2(1488, 1296, 96, 96),
+					new Rect2(144, 912, 192, 144),
+					new Rect2(432, 912, 768, 144),
+					new Rect2(1296, 912, 432, 144),
+					new Rect2(1584, 1056, 144, 192),
+					new Rect2(240, 624, 480, 144),
+					new Rect2(816, 624, 768, 144),
+					new Rect2(432, 432, 96, 240),
+					new Rect2(1344, 432, 96, 240),
+					new Rect2(1344, 432, 96, 240),
+				};
+	}
 	
 	public static void main(String[] args) 
 	{
